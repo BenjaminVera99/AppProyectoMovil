3313371ba961f79a4ef29ba7956dfedbd86924bf
Benjamin <be.veram@duocuc.cl>
2025-10-01 18:33:40 -0300

    mucho avanzado falta HomeScreen

--- app/src/main/java/com/example/appmovil/screens/HomeScreen.kt
+++
@@ -1,2 +0,0 @@
-package com.example.appmovil.screens
-
---
+++ app/src/main/java/com/example/appmovil/ui/screens/HomeScreen.kt
@@ -0,0 +1,12 @@
+package com.example.appmovil.ui.screens
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import com.example.appmovil.viewmodel.MainViewModel
+import com.example.appmovil.viewmodel.ViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+
+fun HomeScreen(){}
