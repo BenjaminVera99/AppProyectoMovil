4ad75a097a00079b1efb17776a6e6e7df9895cb4
benjis <be.veram@duocuc.cl>
2025-10-15 18:00:26 -0300

    guia 10 terminada

--- app/src/main/java/com/example/appmovil/MainActivity.kt
+++ app/src/main/java/com/example/appmovil/MainActivity.kt
@@ -3,16 +3,12 @@ package com.example.appmovil
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -20,6 +16,8 @@ import androidx.navigation.compose.rememberNavController
 import com.example.appmovil.navigation.AppRoute
 import com.example.appmovil.navigation.NavEvent
 import com.example.appmovil.ui.screens.HomeScreen
+import com.example.appmovil.ui.screens.ProfileScreen
+import com.example.appmovil.ui.screens.SettingsScreen
 import com.example.appmovil.ui.theme.AppMovilTheme
 import com.example.appmovil.viewmodels.MainViewModel
 import kotlinx.coroutines.flow.collectLatest
@@ -32,47 +30,56 @@ class MainActivity : ComponentActivity() {
             AppMovilTheme {
                 val viewModel: MainViewModel = viewModel()
                 val navController = rememberNavController()
-                LaunchedEffect(Unit) {
-                viewModel.navigationEvents.collectLatest { event ->
-                    when (event) {
-                        is NavEvent.NavigateTo -> {
-                            navController.navigate(event.route.route) {
-                                event.popUpRoute?.let {
-                                    popUpTo(it.route){
-                                        inclusive = event.inclusive
+
+                LaunchedEffect(key1 = Unit) {
+                    viewModel.navigationEvents.collectLatest { event ->
+                        when (event) {
+                            is NavEvent.NavigateTo -> {
+                                navController.navigate(event.route.route) {
+                                    event.popUpRoute?.let {
+                                        popUpTo(it.route) {
+                                            inclusive = event.inclusive
+                                        }
+                                        launchSingleTop = event.singleTop
+                                        restoreState = true
                                     }
-                                    launchSingleTop = event.singleTop
-                                    restoreState = true
                                 }
                             }
 
-                            is NavEvent.PopBackStack -> navController.popBackStack()
-                            is NavEvent.NavigateUp -> navController.navigateUp()
+
+                                is NavEvent.PopBackStack -> navController.popBackStack()
+                                is NavEvent.NavigateUp -> navController.navigateUp()
+                            }
                         }
                     }
-                }
-            }
-                Scaffold (
-                    modifier = Modifier.fillMaxSize()
-                ) { innerPadding ->
-                    
-                    NavHost(
-                        navController = navController,
-                        startDestination = AppRoute.Home.route,
-                        modifier = Modifier.padding(innerPadding)
-                    ){
-                        composable(route= AppRoute.Home.route) {
-                            HomeScreen(navController = navController, viewModel = viewModel())
-                        }
-                        composable(route= AppRoute.Profile.route) {
-                            HomeScreen(navController = navController, viewModel = viewModel())
-                        }
-                        composable(route= AppRoute.Settings.route) {
-                            HomeScreen(navController = navController, viewModel = viewModel())
+
+                    Scaffold(
+                        modifier = Modifier.fillMaxSize()
+                    ) { innerPadding ->
+
+                        NavHost(
+                            navController = navController,
+                            startDestination = AppRoute.Home.route,
+                            modifier = Modifier.padding(innerPadding)
+                        ) {
+                            composable(route = AppRoute.Home.route) {
+                                HomeScreen(navController = navController, viewModel = viewModel())
+                            }
+                            composable(route = AppRoute.Profile.route) {
+                                ProfileScreen(
+                                    navController = navController,
+                                    viewModel = viewModel()
+                                )
+                            }
+                            composable(route = AppRoute.Settings.route) {
+                                SettingsScreen(
+                                    navController = navController,
+                                    viewModel = viewModel()
+                                )
+                            }
                         }
                     }
                 }
             }
         }
     }
-}
